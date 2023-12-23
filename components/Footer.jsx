5eae7af2df2f6c485333754aac9bee3db8493bc1
Gaven Dcosta <gavendcosta@gmail.com>
2023-12-23 16:34:51 +0530

    final

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -29,23 +29,23 @@ const Footer = () => (
       </div>
 
       <div className="flex flex-col">
-         <div className="mb-[50px] h-[2px] bg-white opacity-10"/>
+        <div className="mb-[50px] h-[2px] bg-white opacity-10"/>
 
-         <div className="flex items-center justify-between flex-wrap gap-4"> 
-            <h4 className="font-extrabold text-[24px] text-white">Metaverses</h4>
-            <p className="font-normal text-[14px] text-white opacity-50">Copyright Metaverses. All rights reserved</p>
+        <div className="flex items-center justify-between flex-wrap gap-4"> 
+          <h4 className="font-extrabold text-[24px] text-white">Metaverses</h4>
+          <p className="font-normal text-[14px] text-white opacity-50">Copyright Metaverses. All rights reserved</p>
 
-            <div className="flex gap-4">
-              {socials.map((social) => (
-                <img
-                  key={social.name}
-                  src={social.url}
-                  alt={social.name}
-                  className="w-[24px] h-[24px] object-contain cursor-pointer"
-                />
-              ))}
-            </div>
-         </div>
+          <div className="flex gap-4">
+            {socials.map((social) => (
+              <img
+                key={social.name}
+                src={social.url}
+                alt={social.name}
+                className="w-[24px] h-[24px] object-contain cursor-pointer"
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   </motion.footer>
--- components/InsightCard.jsx
+++ components/InsightCard.jsx
@@ -9,15 +9,15 @@ const InsightCard = ({ imgUrl, title, subtitle, index }) => (
     className="flex md:flex-row flex-col gap-4"
   >
     <img
-     src={imgUrl}
-     alt="planet-01"
-     className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
+      src={imgUrl}
+      alt="planet-01"
+      className="md:w-[270px] w-full h-[250px] rounded-[32px] object-cover"
     />
 
     <div className="w-full flex justify-between items-center">
       <div className="flex-1 flex flex-col md:ml-[62px] max-w-[650px]">
-         <h4 className="font-normal lg:text-[42px] text-[26px] text-white">{title}</h4>
-         <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">{subtitle}</p>
+        <h4 className="font-normal lg:text-[42px] text-[26px] text-white">{title}</h4>
+        <p className="mt-[16px] font-normal lg:text-[20px] text-[14px] text-secondary-white">{subtitle}</p>
       </div>
 
       <div className="lg:flex hidden items-center justify-center w-[100px] h-[100px] rounded-full bg-transparent border-[1px] border-white">
